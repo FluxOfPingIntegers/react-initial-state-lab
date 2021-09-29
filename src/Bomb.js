85b7ff191e985767399327b0d36f35b14361cf53
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-09-29 11:14:28 -0400

    commit tree 8094015b82c79bb45bc63d80889c8258eb553fbd

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    this.decrement();
+    const stillGoing = <div>{this.state.secondsLeft} seconds left before I go boom!</div>;
+    const explode = <div>Boom!</div>
+    return (!!this.state.secondsLeft > 0 ? stillGoing : explode);
+  }
+
+  decrement = () => {
+    this.setState({
+      secondsLeft: this.state.secondsLeft - 1,
+    })
+  }
+}
+
+export default Bomb
